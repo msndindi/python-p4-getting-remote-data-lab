5e32e02258624118d3fa8c16cfbf20f843675307
Mutindi Hope <msndindi@outlook.com>
2023-06-25 18:50:06 +0300

    Completed Assignment

--- lib/GetRequester.py
+++ lib/GetRequester.py
@@ -7,7 +7,19 @@ class GetRequester:
         self.url = url
 
     def get_response_body(self):
-        pass
+        URL = "https://learn-co-curriculum.github.io/json-site-example/endpoints/people.json"
+
+        response = requests.get(URL)
+        return response.content
+
 
     def load_json(self):
-        pass
+        response_data = json.loads(self.get_response_body())
+        return response_data
+
+
+
+
+
+
+       
